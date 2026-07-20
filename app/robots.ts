b9fd86bfc2f3a52b3e8cd6e67ce03306350013ff
Dra Paloma Oliveira Odontologia <dra.palomaoliveira.odontologia@gmail.com>
2026-07-20 13:04:39 -0300

    Adicionar robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,11 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://site-clinica-paloma-xg6q.vercel.app";
-
   return {
     rules: {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: `${baseUrl}/sitemap.xml`,
+    sitemap: "https://site-clinica-paloma-xg6q.vercel.app/sitemap.xml",
   };
 }
